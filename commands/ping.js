c991e74fc75f95f207a1f59b896421073406ca7d
MAHIYA__BOTS <161313291+newtonwwmp@users.noreply.github.com>
2025-01-29 16:28:47 +0530

    PRINCESS _ UMANDA

---
+++ commands/ping.js
@@ -0,0 +1,19 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+zokou({ nomCom: 'ping',
+    desc: 'To check ping',
+    Categorie: 'General',
+    reaction: '🚨', 
+    fromMe: 'true', 
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+    const { start } = new Date().getTime();
+    return repondre('🔨 *PRINCESS_UMANDA PING*\n ```' + 2000 + '``` *ms*');
+    const { end } = new Date().getTime();
+    await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*');
+  }
+);
