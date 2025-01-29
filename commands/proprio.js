812bb11b4b98ee5a81140aca700909d6f7a0fbe5
MAHIYA__BOTS <161313291+newtonwwmp@users.noreply.github.com>
2025-01-29 16:33:02 +0530

    PRINCESS _ UMANDA

---
+++ commands/proprio.js
@@ -0,0 +1,17 @@
+const { zokou } = require("../framework/zokou");
+
+zokou({ nomCom: "reboot", categorie: "Mods", reaction: "👨🏿‍💼" }, async (dest, z, com) => {
+
+  const { repondre, ms, dev, superUser } = com;
+
+  if (!superUser) {
+    return repondre("This command is for owner only");
+  }
+
+  const { exec } = require("child_process");
+
+  repondre("*restarting ...*");
+
+  exec("pm2 restart all");
+
+});
