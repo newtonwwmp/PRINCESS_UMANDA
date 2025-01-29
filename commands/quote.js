f8f0340a5bc0fe025d8273d1f1df9b2da597a12d
MAHIYA__BOTS <161313291+newtonwwmp@users.noreply.github.com>
2025-01-29 16:35:01 +0530

    QUEEN-AMANDA

---
+++ commands/quote.js
@@ -0,0 +1,49 @@
+const { zokou } = require('../framework/zokou');
+
+zokou({ nomCom: 'quote', categorie: 'Fun' }, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, verifGroupe, arg } = commandeOptions;
+  if (!verifGroupe) {
+    repondre('Commande réservée au groupe uniquement');
+    return;
+  }
+
+  if (!arg[0]) {
+    try {
+      fetch('https://animechan.xyz/api/random')
+        .then((response) => response.json())
+        .then(async (quote) => {
+          repondre(`╔══════════════════════════╗
+║   PRINCESS_UMANDA         ║
+╚══════════════════════════╝
+
+🎬 Anime: ${quote.anime}
+👤 Character: ${quote.character}
+💬 Quote: ${quote.quote}
+
+Powered by *⚔ MIHIRANGA ⚔*`);
+        });
+    } catch (e) {
+      repondre('Erreur lors de la génération de la citation : ' + e.message);
+    }
+  } else {
+    const query = arg.join(' ');
+
+    try {
+      fetch('https://animechan.xyz/api/random/character?name=' + query)
+        .then((response) => response.json())
+        .then(async (quote) => {
+          repondre(`╔══════════════════════════╗
+║   PRINCESS_UMANDA         ║
+╚══════════════════════════╝
+
+🎬 Anime: ${quote.anime}
+👤 Character: ${quote.character}
+💬 Quote: ${quote.quote}
+
+Powered by PRINCESS_UMANDA`);
+        });
+    } catch (e) {
+      repondre('Erreur lors de la génération de la citation : ' + e.message);
+    }
+  }
+});
