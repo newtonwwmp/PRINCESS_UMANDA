50f602fe5b38f8989d4c91c532c36cd8f4cae1f5
MAHIYA__BOTS <161313291+newtonwwmp@users.noreply.github.com>
2025-01-31 22:22:08 +0530

    PRINCESS _ UMANDA

--- commands/youtube.js
+++ commands/youtube.js
@@ -2,9 +2,8 @@ const { zokou } = require("../framework/zokou");
 const yts = require('yt-search');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
-const yt = require("../framework/dl/ytdl-core.js");
 const ffmpeg = require("fluent-ffmpeg");
-const yts1 = require("youtube-yts");
+const path = require("path");
 
 zokou({
   nomCom: "song",
@@ -23,94 +22,59 @@ zokou({
     const search = await yts(topo);
     const videos = search.videos;
 
-    if (videos && videos.length > 0 && videos[0]) {
-      const urlElement = videos[0].url;
-
-      let infoMess = {
-        image: { url: videos[0].thumbnail },
-        caption: `\n*Song name:* _${videos[0].title}_\n*Time:* _${videos[0].timestamp}_\n*URL:* _${videos[0].url}_\n\n_*PRINCESS_UMANDA SONG DOWNLOADING...*_\n\n`
-      };
-
-      zk.sendMessage(origineMessage, infoMess, { quoted: ms });
-
-      // Obtain audio stream from video
-      const audioStream = ytdl(urlElement, { filter: 'audioonly', quality: 'highestaudio' });
-
-      // Save the audio file locally
-      const filename = './audio.mp3';
-      const fileStream = fs.createWriteStream(filename);
-      audioStream.pipe(fileStream);
-
-      fileStream.on('finish', () => {
-        // Send the audio file
-        zk.sendMessage(origineMessage, { audio: { url: filename }, mimetype: 'audio/mp3' }, { quoted: ms, ptt: false });
-        console.log("Audio file sent!");
-      });
-
-      fileStream.on('error', (error) => {
-        console.error('Error writing audio file:', error);
-        repondre('An error occurred while writing the audio file.');
-      });
-    } else {
+    if (!videos || videos.length === 0) {
       repondre('No video found.');
+      return;
     }
-  } catch (error) {
-    console.error('Error during search or download:', error);
-    repondre('An error occurred during search or download.');
-  }
-});
 
-zokou({
-  nomCom: "video",
-  categorie: "Search",
-  reaction: "🎥"
-}, async (origineMessage, zk, commandeOptions) => {
-  const { arg, ms, repondre } = commandeOptions;
+    const video = videos[0];
+    const urlElement = video.url;
 
-  if (!arg[0]) {
-    repondre("Insert video name.");
-    return;
-  }
+    let infoMess = {
+      image: { url: video.thumbnail },
+      caption: `🎵 *Song:* _${video.title}_\n⏳ *Duration:* _${video.timestamp}_\n🔗 *URL:* _${video.url}_\n\n🎧 _PRINCESS_UMANDA SONG DOWNLOADING..._\n`
+    };
 
-  const topo = arg.join(" ");
-  try {
-    const search = await yts(topo);
-    const videos = search.videos;
+    zk.sendMessage(origineMessage, infoMess, { quoted: ms });
 
-    if (videos && videos.length > 0 && videos[0]) {
-      const Element = videos[0];
+    // File paths
+    const tempFilePath = path.join(__dirname, "temp_audio.mp4");
+    const outputFilePath = path.join(__dirname, "audio.mp3");
 
-      let InfoMess = {
-        image: { url: videos[0].thumbnail },
-        caption: `*Video name:* _${Element.title}_\n*Time:* _${Element.timestamp}_\n*URL:* _${Element.url}_\n\n_*PRINCESS_UMANDA On downloading...*`
-      };
+    // Download audio
+    const audioStream = ytdl(urlElement, { filter: 'audioonly', quality: 'highestaudio' });
 
-      zk.sendMessage(origineMessage, InfoMess, { quoted: ms });
+    const fileStream = fs.createWriteStream(tempFilePath);
+    audioStream.pipe(fileStream);
 
-      // Get video info from YouTube link
-      const videoInfo = await ytdl.getInfo(Element.url);
-      // Choose the best available video format
-      const format = ytdl.chooseFormat(videoInfo.formats, { quality: '18' });
-      // Start downloading the video stream
-      const videoStream = ytdl.downloadFromInfo(videoInfo, { format });
+    fileStream.on('finish', () => {
+      console.log("Download complete! Converting to MP3...");
 
-      // Save the video file locally
-      const filename = './video.mp4';
-      const fileStream = fs.createWriteStream(filename);
-      videoStream.pipe(fileStream);
+      // Convert to MP3 using ffmpeg
+      ffmpeg(tempFilePath)
+        .toFormat("mp3")
+        .on("end", () => {
+          console.log("MP3 conversion finished!");
 
-      fileStream.on('finish', () => {
-        // Send the video file
-        zk.sendMessage(origineMessage, { video: { url: filename }, caption: "*PRINCESS_UMANDA*", gifPlayback: false }, { quoted: ms });
-      });
+          // Send the audio file
+          zk.sendMessage(origineMessage, { audio: { url: outputFilePath }, mimetype: 'audio/mp3' }, { quoted: ms, ptt: false });
 
-      fileStream.on('error', (error) => {
-        console.error('Error writing video file:', error);
-        repondre('An error occurred while writing the video file.');
-      });
-    } else {
-      repondre('No video found.');
-    }
+          // Delete temp files after sending
+          fs.unlinkSync(tempFilePath);
+          fs.unlinkSync(outputFilePath);
+        })
+        .on("error", (err) => {
+          console.error("FFmpeg error:", err);
+          repondre("An error occurred while converting the audio.");
+        })
+        .save(outputFilePath);
+    });
+
+    fileStream.on('error', (error) => {
+      console.error('Error writing audio file:', error);
+      repondre('An error occurred while writing the audio file.');
+    });
+
   } catch (error) {
     console.error('Error during search or download:', error);
     repondre('An error occurred during search or download.');
