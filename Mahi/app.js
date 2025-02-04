0872ff2549ff842c17d83b71b1a1d96ab538247b
MAHIYA__BOTS <161313291+newtonwwmp@users.noreply.github.com>
2025-02-04 11:09:00 +0530

    PRINCESS _ UMANDA

---
+++ Mahi/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  'value': true
+});
+exports.reagir = undefined;
+async function reagir(_0x1ce24f, _0x100565, _0x89dc09, _0x567598) {
+  await _0x100565.sendMessage(_0x1ce24f, {
+    'react': {
+      'text': _0x567598,
+      'key': _0x89dc09.key
+    }
+  });
+}
+exports.reagir = reagir;
